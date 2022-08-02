8388be7362f7539ac82c8d58f0b36d1f2b7b79e3
Pedro <ussanezio@hotmail.com>
2022-08-02 07:23:34 -0300

    att

--- app.js
+++ app.js
@@ -9,19 +9,3 @@ modoescuroclaro.onclick = function(){
 alternarmodo.onclick = function(){  
     alternarmodo.classList.toggle('active')
 }
-
-
-const cursor = document.querySelector('.cursor'),
-    anchor = document.querySelectorAll("a");
-    document.addEventListener('mousemove', (e) =>{
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
-    });   
-    anchor.forEach((anc) =>{
-        anc.addEventListener("mouseover", () =>{
-            cursor.style.transform = "scale(2)";
-    });
-        anc.addEventListener("mouseleave", () =>{
-            cursor.style.transform ="";
-    });
-    });
